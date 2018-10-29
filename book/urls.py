1aea0b5a31b1f2fe3b2928e90ba78036d8a45427
zhaokai <1098802760@qq.com>
2018-10-29 09:26:01 +0800

    1.manage图书管理页面 2.read读者管理页面

--- book/urls.py
+++ book/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 from book import views
 
 urlpatterns = [
-    url(r'^$' ,views.login_view),
-    url(r'^bookmanage' ,views.bookmanage_view),
-
+    url(r'^$' ,views.bookmanage_view),
+    url(r'login/' ,views.login_view),
+    url(r'bookmanage/', views.manage_view),
+    url(r'readmanage/', views.reader_view),
 ]
--- book/views.py
+++ book/views.py
@@ -5,24 +5,34 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from book.models import User
+from book.models import *
 
-
+#首页
 def bookmanage_view(request):
 
-    return render(request, 'bookmanage.html')
+    books = Book.objects.all()
+    return render(request, 'bookmanage.html', {'books': books})
 
-
+# 登录
 def login_view(request):
     if request.method == 'GET':
         return render(request, 'login.html')
     else:
-        luname = request.POST.get('luname','')
-        lpwd = request.POST.get('lpwd','')
-        count = User.objects.filter(uname=luname ,psd=lpwd).count()
+        luname = request.GET.get('luname', '')
+        lpwd = request.GET.get('lpwd', '')
+        count = User.objects.filter(uname=luname, psd=lpwd).count()
         if count == 1:
             return render(request, 'bookmanage.html')
         else:
             return HttpResponse('321')
 
+#图书管理
+def manage_view(request):
+    books = Book.objects.all()
+    return render(request, 'manage.html', {'books': books})
 
+
+def reader_view(request):
+    reads = Reader.objects.all()
+
+    return render(request,'read.html',{'reads':reads})
