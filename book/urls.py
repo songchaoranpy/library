ed0a7c3272f680db607213a9a00810a331297bae
zhaokai <1098802760@qq.com>
2018-10-31 10:01:54 +0800

    图书借阅可用

--- book/urls.py
+++ book/urls.py
@@ -4,9 +4,17 @@ from django.conf.urls import url
 from book import views
 
 urlpatterns = [
-    url(r'^$' ,views.bookmanage_view),
-    url(r'login/' ,views.login_view),
-    url(r'bookmanage/', views.manage_view),
-    url(r'readmanage/', views.reader_view),
+    url(r'^$' ,views.bookmanage),
+    url(r'^index/' ,views.index),
+    url(r'login/' ,views.login),
+    url(r'bookmanage/', views.manage),
+    url(r'readmanage/', views.reader),
+    url(r'addbook/', views.addbook),
+    url(r'booktype/', views.booktype),
+    url(r'addtype/', views.addtype),
+    url(r'addread/', views.addread),
+    url(r'^bookBorrow/', views.bookBorrow),
+    url(r'^bookRenew/', views.bookRenew),
+    url(r'^bookBack/', views.bookBack),
 
 ]
--- book/views.py
+++ book/views.py
@@ -8,13 +8,27 @@ from django.shortcuts import render
 from book.models import *
 
 #首页
-def bookmanage_view(request):
+def bookmanage(request):
 
     books = Book.objects.all()
     return render(request, 'bookmanage.html', {'books': books})
 
-# 登录
-def login_view(request):
+#登录
+def index(request):
+    if request.method == 'GET':
+        return render(request,'index.html')
+    else:
+        uname = request.POST.get('name','')
+        upwd = request.POST.get('pwd','')
+        cont = User.objects.filter(name=uname,psd=upwd).count()
+        if  cont:
+            return render(request,'bookmanage.html')
+        else:
+            return HttpResponse('<script>alert("用户名或密码有误");location.href="/index/"</script>')
+
+
+#更改管理员
+def login(request):
     if request.method == 'GET':
         return render(request, 'login.html')
     else:
@@ -27,12 +41,79 @@ def login_view(request):
             return render(request,'login.html')
 
 #图书管理
-def manage_view(request):
+def manage(request):
     books = Book.objects.all()
     return render(request, 'manage.html', {'books': books})
 
 
-def reader_view(request):
-    reads = Reader.objects.all()
+#添加图书
+def addbook(request):
+    if request.method == "GET":
+        return render(request,'addbook.html')
+    else:
+        bname = request.POST.get('bookname','')
+        bpress = request.POST.get('press','')
+        bauthor = request.POST.get('author','')
+        btype = request.POST.get('booktype','')
+        bcount = request.POST.get('bookcount','')
+        if bname and bpress and bauthor and btype and bcount:
+            books = Book(bookname=bname,press=bpress,author=bauthor,type_id=btype,count=bcount)
+            books.save()
+            return render(request,'successful.html')
+        else:
+            return render(request,'failure.html')
 
-    return render(request,'read.html',{'reads':reads})
+#图书类型展示
+def booktype(request):
+    types = Type.objects.all()
+    return render(request,'booktype.html',{'types':types})
+
+#添加图书类型
+def addtype(request):
+    if request.method == 'GET':
+        return render(request, 'addtype.html')
+    else:
+        tname = request.POST.get('typename','')
+        if tname:
+            type = Type(type=tname)
+            type.save()
+            return render(request,'successful.html')
+        else:
+            return render(request,'failure.html')
+
+# 读者管理
+def reader(request):
+    reads = Reader.objects.all()
+    return render(request, 'read.html', {'reads': reads})
+
+
+
+#添加读者
+def addread(request):
+    if request.method == 'GET':
+        reads = Reader.objects.all()
+        return render(request, 'addread.html', {'reads': reads})
+    else:
+        rname = request.POST.get('rname','')
+        rtel = request.POST.get('rtel','')
+        if rname and rtel:
+            read = Reader(rname=rname,tel=rtel)
+            read.save()
+            return render(request,'successful.html')
+        else:
+            return render(request, 'failure.html')
+
+#借阅
+def bookBorrow(request):
+    if request.method == 'GET':
+        return render(request,'bookBorrow.html')
+    else:
+        return HttpResponse('123')
+
+#续借
+def bookRenew(request):
+    return render(request,'bookRenew.html')
+
+#归还
+def bookBack(request):
+    return render(request,'bookBack.html')
